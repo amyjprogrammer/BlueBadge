a94cff38a3d28bf5adb13fb94379c448090c601f
Jeffrey Spurling <89798840+JeffySpur@users.noreply.github.com>
2021-10-26 09:55:25 -0400

    Added PollChoice Class and added PollChoiceListItem

---
+++ BlueBadge.Data/PollChoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Data
+{
+    public class PollChoice
+    {
+        [Key]
+        public int PollId { get; set; } 
+        
+        [ForeignKey(nameof(Question))]
+        public int QuestionId { get; set; }
+        public List<PollChoice> Choices { get; set; }
+    }
+}
+
