e37403e8c0531f6c389067c4682148530390aa49
Taylor <taylor.randol@gmail.com>
2021-10-27 10:48:33 -0400

    Response Scaffold

---
+++ BlueBadge.Services/ResponseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Services
+{
+    public class ResponseService
+    {
+        private readonly Guid _userId;
+
+        public ResponseService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+    }
+}
