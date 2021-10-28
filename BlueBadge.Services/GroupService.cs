eec9c6331e17d490e28986619a42f7f4b03f80f4
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-28 15:05:17 -0400

    get all and create for Group class

---
+++ BlueBadge.Services/GroupService.cs
@@ -0,0 +1,55 @@
+﻿using BlueBadge.Data;
+using BlueBadge.Models.Group.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Services
+{
+    public class GroupService
+    {
+        private readonly Guid _userId;
+
+        public GroupService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateGroup(GroupCreate model)
+        {
+            var entity =
+                new Group()
+                {
+                    CustomerId = _userId,
+                    GroupName = model.GroupName
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Groups.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<GroupListItem> GetGroups()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Groups
+                        .Where(e => e.CustomerId == _userId)
+                        .Select(
+                            e =>
+                            new GroupListItem
+                            {
+                                GroupName = e.GroupName //need to figure out how to display the info for questions and emails
+                            }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
