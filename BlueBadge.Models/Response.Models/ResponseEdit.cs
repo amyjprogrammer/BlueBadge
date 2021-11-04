47ccb8177d18f8ebcfa83a68d03bf40c3c24ac41
Taylor <taylor.randol@gmail.com>
2021-11-04 08:59:59 -0400

    Response Rebuild

---
+++ BlueBadge.Models/Response.Models/ResponseEdit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Models.Response.Models
+{
+    public class ResponseEdit
+    {
+        public int ResponseId { get; set; }
+        public int PollId { get; set; }
+    }
+}
---
+++ BlueBadge.Models/Response.Models/ResponseListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Models.Response.Models
+{
+    public class ResponseListItem
+    {
+        [Display(Name = "Response Id Number")]
+        public int ResponseId { get; set; }
+
+        [Display(Name = "Poll Id Number")]
+        public int PollId { get; set; }
+
+        [Display(Name = "Times Selected")]
+        public int Selected { get; set; }
+    }
+}
